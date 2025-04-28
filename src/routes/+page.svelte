b5d5fbbf6e4db8768e3469da316d04df5903dbc4
Slakoth <69316271+t84@users.noreply.github.com>
2025-04-28 17:38:41 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <main class="text-white min-h-screen py-10">
   <div class="max-w-4xl mx-auto px-6">
     <header class="text-left">
-      <h1 class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r text-white mb-4 mt-[100px]">
+      <h1 class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r text-white mb-4 mt-[75px]">
         Hey I'm Joshua
       </h1>
       <a aria-hidden="true" target="_blank" href="https://go.joshua.moe/github">Github</a>
